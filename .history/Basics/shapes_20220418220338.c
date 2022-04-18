ddd6309ad4b12160e5d14e3ea3096aebbbdeae2d
Programmer-RD-AI <go2ranuga@gmail.com>
2022-04-18 22:04:58 +0530

    Update shapes.c
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ .history/Basics/shapes_20220418220338.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("/__|");
+    printf("/__|\n");
+    printf("/__|");
+}
---
+++ .history/Basics/shapes_20220418220359.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf(" /_|");
+    printf(" /_|");
+    printf("/__|\n");
+}
