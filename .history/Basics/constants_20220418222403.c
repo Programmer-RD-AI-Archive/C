4c9788822d87fba7a6a2e912ae4fcabe655f18a9
Programmer-RD-AI <go2ranuga@gmail.com>
2022-04-18 22:24:40 +0530

    Update
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ .history/Basics/constants_20220418222403.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#define 
+int main()
+{
+    const int num = 5;
+    return 0;
+}
---
+++ .history/Basics/constants_20220418222406.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#define num 5
+int main()
+{
+    const int num = 5;
+    return 0;
+}
