6135f47543dbd6cc7ce109b77d56cfbc3d3d6acb
Programmer-RD-AI <go2ranuga@gmail.com>
2022-04-19 07:37:39 +0530

    Update
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- Basics/getting_user_informations.c
+++ Basics/getting_user_informations.c
@@ -4,7 +4,7 @@ int main()
 {
     int age;
     printf("Enter your age : ");
-    scanf("%d", &age);
+    scanf("%d", &age); // & because its pointing to the variables address I think... :)
     printf("%d", age);
     return 0;
 }
