56bb8fdc6e25717e063d8a9d03de114d3e1250de
Programmer-RD-AI <go2ranuga@gmail.com>
2022-04-19 11:54:46 +0530

    Update
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ .history/Basics/memory_address_20220419115312.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 30;
+    double gpa = 3.4;
+    char grade = 'A';
+    printf("%p", &age); // This is the place in memory that the variable age is stored in
+    return 0;
+}
