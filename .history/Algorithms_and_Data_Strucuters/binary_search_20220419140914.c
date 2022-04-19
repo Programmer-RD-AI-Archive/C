ec4cd18c76e2c0147d8e204c6fe090057b9a98fb
Programmer-RD-AI <go2ranuga@gmail.com>
2022-04-19 14:33:26 +0530

    Update
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ .history/Algorithms_and_Data_Strucuters/binary_search_20220419140914.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(){
+    int unsorted[] = {9,5,13,3,8,7,2,12,6,10,4,11,1}
+}
---
+++ .history/Algorithms_and_Data_Strucuters/binary_search_20220419141656.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int binary_search(int arr[], int val, int l_idx, int r_idx);
+int main()
+{
+    int unsorted[] = {9, 5, 13, 3, 8, 7, 2, 12, 6, 10, 4, 11, 1};
+    int sorted[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    binary_search(sorted, 12, 0, 12);
+    return 0;
+}
+int binary_search(int arr[], int val, int l_idx, int r_idx)
+{
+    int mid = l_idx + (r_idx - l_idx) / 2;
+    if (arr[mid] == val)
+    {
+        return mid;
+    }
+    else if (arr[mid] > val)
+    {
+        return binary_search(arr, val, l_idx, mid - 1);
+    }
+    else{
+        return binary_search(arr, val, mid - 1,)
+    }
+}
