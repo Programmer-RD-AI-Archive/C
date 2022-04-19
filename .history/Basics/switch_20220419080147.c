66dac033c4d78b36cb29c2e942bd49b7499118ff
Programmer-RD-AI <go2ranuga@gmail.com>
2022-04-19 11:34:17 +0530

    Update
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ .history/Basics/switch_20220419080147.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int test = 1;
+    switch (test){
+        case 1:
+            printf("1")
+    }
+}
---
+++ .history/Basics/switch_20220419080151.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int test = 1;
+    switch (test)
+    {
+    case 1:
+        printf("1");
+    }
+}
