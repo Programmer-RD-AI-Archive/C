e477c2fdc1cc25fca18c3b95876767e87ad2a96d
Programmer-RD-AI <go2ranuga@gmail.com>
2022-04-19 14:06:13 +0530

    Update
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ .history/Algorithms_and_Data_Strucuters/linear_search_20220419140220.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+    int Arr[4] = {2,4,5,6}
+}
---
+++ .history/Algorithms_and_Data_Strucuters/linear_search_20220419140246.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int LinearSearch(int Arr[], int size) int main()
+{
+    int Arr[4] = {2, 4, 6, 8};
+    return 0;
+}
